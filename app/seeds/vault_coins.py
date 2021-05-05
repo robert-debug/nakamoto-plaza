e275065b7203805fe6c271c010f3e32f2b882765
Robert George <rgeorge1982@gmail.com>
2021-05-05 19:46:04 -0400

    seeding works coin routes up

--- app/seeds/vault_coins.py
+++ app/seeds/vault_coins.py
@@ -1,4 +1,5 @@
 from app.models import db, VaultCoin
+import random
 
 def seed_vault_coins():
     i = 1
@@ -11,9 +12,9 @@ def seed_vault_coins():
     db.session.add(two)
 
     while i < 14:
-        coin_select_one= (2, 3)
-        coin_select_two= (4, 9)
-        coin_select_three= (10, 12)
+        coin_select_one= random.randint(2, 3)
+        coin_select_two= random.randint(4, 9)
+        coin_select_three= random.randint(10, 12)
         
 
         bitcoin= VaultCoin (
@@ -27,7 +28,7 @@ def seed_vault_coins():
         vault_coin1= VaultCoin (
             vault_id= i,
             coin_id= coin_select_one,
-            amount= float(randint(1, 9))
+            amount= float(random.randint(1, 9))
         )
 
         db.session.add(vault_coin1)
@@ -35,13 +36,13 @@ def seed_vault_coins():
         vault_coin2= VaultCoin (
             vault_id= i,
             coin_id= coin_select_two,
-            amount= float(randint(1, 9))
+            amount= float(random.randint(1, 9))
         )
         db.session.add(vault_coin2)
         vault_coin3= VaultCoin (
             vault_id= i,
             coin_id= coin_select_three,
-            amount= float(randint(1, 9))
+            amount= float(random.randint(1, 9))
         )
         db.session.add(vault_coin3)
 
