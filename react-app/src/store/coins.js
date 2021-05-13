fb27a955821644420f715177d3db8c04b348e1b7
Robert George <rgeorge1982@gmail.com>
2021-05-13 11:25:26 -0400

    undid no cors

--- react-app/src/store/coins.js
+++ react-app/src/store/coins.js
@@ -38,10 +38,7 @@ export const requestUserCoins = (userId) => async(dispatch)=> {
 
 export const requestOneCoin = (symbol) => async(dispatch) => {
     console.log(symbol)
-    const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=2dea8624d0f169a05115d37d8ed28cc2&ids=${symbol}&interval=1h,1d&convert=USD&per-page=100&page=1`,
-    {
-        mode: 'no-cors'
-    })
+    const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=2dea8624d0f169a05115d37d8ed28cc2&ids=${symbol}&interval=1h,1d&convert=USD&per-page=100&page=1`)
     const coin = await response.json()
     dispatch(getOneCoin(coin))
 }
